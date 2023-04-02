c5066fe59648219c58cb32ffadb878906f66c5e3
Tedla2001 <tedlamariye99@outlook.com>
2023-04-02 00:11:15 -0400

    intial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const http=require('http');
+const server=http.createServer().listen(8080);
+// const process=require('./lib/process.js');
+const POST=require('./lib/post.js');
+const DELETE=require('./lib/delete.js');
+
+
+server.on('request',async(req,res)=>{
+	switch(req.method){
+		case "GET":
+			GET(req,res);
+			break;
+		case "POST":
+			POST(req,res);
+			break;
+		case "PUT":
+			PUT(req,res);
+			break;
+		case "DELETE":
+			DELETE(req,res);
+			break;
+	}
+});
+
+  
+
+  
+  
+  
+
+
